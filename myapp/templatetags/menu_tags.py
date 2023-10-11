f6180b04f8b9793fddc78993f4179c84659616e5
Ilya Rogov <ilya.r.2003@mail.ru>
2023-10-12 02:10:17 +0600

    v1.0

--- myapp/templatetags/menu_tags.py
+++ myapp/templatetags/menu_tags.py
@@ -1,11 +1,9 @@
 from django import template
-from django.urls import reverse
 from myapp.models import MenuItem
-
 
 register = template.Library()
 
-@register.inclusion_tag('myapp/menu.html', takes_context=True)
-def draw_menu(context):
-    menu_items = MenuItem.objects.all()
+@register.inclusion_tag('myapp/menu.html')
+def draw_menu(menu_name):
+    menu_items = MenuItem.objects.filter(parent=None)
     return {'menu_items': menu_items}
